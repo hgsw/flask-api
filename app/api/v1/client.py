fca16dfb5abca25dfd6c9e8cbc241b78c206e274
houshiwei <hsw93@163.com>
2024-07-01 15:20:23 +0800

    二次重构/v1/client.py的register接口，简化参数校验

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -4,7 +4,7 @@ from flask import request
 from app.validators.form import ClientForm
 from app.validators.form import UserEmailForm
 from app.models.user import User
-from app.libs.error_code import ClientTypeError
+from app.libs.error_code import Success
 
 api = Redprint("client")
 
@@ -13,17 +13,13 @@ api = Redprint("client")
 def register():
     """不要将user局限成人，三方、APP、小程序、用户等，因此注册的形式非常多
     参数、校验、接收参数"""
-    data = request.json
-    form = ClientForm(data=data)
-    form.validate_for_api()
+    form = ClientForm().validate_for_api()
     promise = {ClientTypeEnum.USER_EMAIL: __resgister_user_by_email}
     promise[form.type.data]()
 
-    return "register success"
+    return Success()
 
 
 def __resgister_user_by_email():
-    data = request.json
-    form = UserEmailForm(data=data)
-    if form.validate():
-        User.register_by_email(form.nickname.data, form.account.data, form.secret.data)
+    form = UserEmailForm().validate_for_api()
+    User.register_by_email(form.nickname.data, form.account.data, form.secret.data)
--- app/validators/base.py
+++ app/validators/base.py
@@ -1,14 +1,19 @@
 from wtforms import Form
+from flask import request
 from app.libs.error_code import ParameterException
+
 
 class BaseForm(Form):
     """使用是自定义的BaseForm进行参数校验"""
 
-    def __init__(self, data):
-        # data需要校验的参数
+    def __init__(self):
+        # data需要校验的参数，参数校验的基类获取请求参数
+        data = request.json
         super(BaseForm, self).__init__(data=data)
 
     def validate_for_api(self):
         valid = super(BaseForm, self).validate()
         if not valid:
             raise ParameterException(msg=self.errors)
+        
+        return self
