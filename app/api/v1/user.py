5de64352026a7bb20a63e9400c7e2646b72eb002
houshiwei <hsw93@163.com>
2024-07-03 20:28:21 +0800

    解决delete_user超权问题

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, g
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
 from app.models.user import User
@@ -22,9 +22,14 @@ def get_user(uid):
     return jsonify(user)
 
 
-@api.route("/<int:uid>", methods=["DELETE"])
+@api.route("", methods=["DELETE"])
 @auth.login_required
-def delete_user(uid):
+def delete_user():
+    # 超权限（用户可以访问改接口是否拥有删除的操作）
+    # 用户1、2， 用户1token验证通过后，携带uid=2，就有可能删除用户2
+    # 不应该由用户传递uid删除用户，解决是从token中读取
+    # 在验证token的时候，use信息已存在g变量中，g变量是线程隔离的
+    uid = g.user.uid
     with db.auto_commit():
         user = User.query.filter_by(id=uid).first_or_404()
         user.delete()
