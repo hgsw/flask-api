f8d0ca9009dbafa16a6675c7f0c96051179aa729
houshiwei <hsw93@163.com>
2024-07-05 17:18:06 +0800

    增加token查询接口

--- app/api/v1/token.py
+++ app/api/v1/token.py
@@ -28,3 +28,28 @@ def generate_auth_token(uid, ac_type, scope=None, expiration=7200):
     s = Serializer(current_app.config["SECRET_KEY"], expires_in=expiration)
 
     return s.dumps({"uid": uid, "type": ac_type.value, "scope": scope})
+
+
+@token.route("/secret", methods=["POST"])
+def get_token_info():
+    """获取令牌信息"""
+    form = TokenForm().validate_for_api()
+    s = Serializer(current_app.config["SECRET_KEY"])
+    try:
+        data = s.loads(form.token.data, return_header=True)
+    except SignatureExpired:
+        raise AuthFailed(msg="token is expired", error_code=1003)
+    except BadSignature:
+        raise AuthFailed(msg="token is invalid", error_code=1002)
+
+    r = {
+        "scope": data[0]["scope"],
+        "create_at": date_str(data[1]["iat"]),
+        "expire_in": date_str(data[1]["exp"]),
+        "uid": data[0]["uid"],
+    }
+    return jsonify(r)
+
+
+def date_str(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
